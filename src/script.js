b3ad10bd226a01c573a9f5731cc3ec36562bfc22
Cheri Marie Simmons <cheri_simmons@outlook.com>
2023-11-14 23:07:33 -0500

    * integrated yesterday/today/tomorrow comparison function
    checkpoint

--- src/script.js
+++ src/script.js
@@ -6,6 +6,19 @@ setInterval(function () {
   let knoxvilleDate = moment().tz("US/Eastern").format("ddd, MMM D, YYYY");
   let knoxvilleAmPm = moment().tz("US/Eastern").format("A");
   let knoxvilleTodayElement = document.querySelector("#knoxville-today");
+  let userTime = moment().format("ddd, MMM D, YYYY");
+
+  if (knoxvilleDate === userTime) {
+    knoxvilleTodayElement.innerHTML = "TODAY";
+  } else {
+    if (knoxvilleDate < userTime) {
+      knoxvilleTodayElement.innerHTML = "YESTERDAY";
+    } else {
+      if (knoxvilleDate > userTime) {
+        knoxvilleTodayElement.innerHTML = "TOMORROW";
+      }
+    }
+  }
 
   knoxvilleTimeElement.innerHTML = knoxvilleTime;
   knoxvilleDateElement.innerHTML = knoxvilleDate;
@@ -21,6 +34,20 @@ setInterval(function () {
     .tz("Australia/Brisbane")
     .format("ddd, MMM D, YYYY");
   let brisbaneAmPm = moment().tz("Australia/Brisbane").format("A");
+  let brisbaneTodayElement = document.querySelector("#brisbane-today");
+  let userTime = moment().format("ddd, MMM D, YYYY");
+
+  if (brisbaneDate === userTime) {
+    brisbaneTodayElement.innerHTML = "TODAY";
+  } else {
+    if (brisbaneDate < userTime) {
+      brisbaneTodayElement.innerHTML = "YESTERDAY";
+    } else {
+      if (brisbaneDate > userTime) {
+        brisbaneTodayElement.innerHTML = "TOMORROW";
+      }
+    }
+  }
 
   brisbaneTimeElement.innerHTML = brisbaneTime;
   brisbaneDateElement.innerHTML = brisbaneDate;
