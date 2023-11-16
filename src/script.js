6b6fefc816a18e85946f788d3396c29387323950
Cheri Marie Simmons <cheri_simmons@outlook.com>
2023-11-16 13:44:33 -0500

    * fixed today/tomorrow functionality

--- src/script.js
+++ src/script.js
@@ -1,55 +1,29 @@
 setInterval(function () {
-  let knoxvilleTimeElement = document.querySelector("#knoxville-time");
-  let knoxvilleDateElement = document.querySelector("#knoxville-date");
-  let knoxvilleAmPmElement = document.querySelector("#knoxville-am-pm");
-  let knoxvilleTime = moment().tz("US/Eastern").format("h:mm:ss");
-  let knoxvilleDate = moment().tz("US/Eastern").format("ddd, MMM D, YYYY");
-  let knoxvilleAmPm = moment().tz("US/Eastern").format("A");
-  let knoxvilleTodayElement = document.querySelector("#knoxville-today");
-  let userTime = moment().format("ddd, MMM D, YYYY");
-
-  if (knoxvilleDate === userTime) {
-    knoxvilleTodayElement.innerHTML = "Today";
-  } else {
-    if (knoxvilleDate < userTime) {
-      knoxvilleTodayElement.innerHTML = "Yesterday";
-    } else {
-      if (knoxvilleDate > userTime) {
-        knoxvilleTodayElement.innerHTML = "Tomorrow";
-      }
-    }
-  }
-
-  knoxvilleTimeElement.innerHTML = knoxvilleTime;
-  knoxvilleDateElement.innerHTML = knoxvilleDate;
-  knoxvilleAmPmElement.innerHTML = knoxvilleAmPm;
-}, 1000);
-
-setInterval(function () {
   let brisbaneTimeElement = document.querySelector("#brisbane-time");
+  let brisbaneDayElement = document.querySelector("#brisbane-day");
   let brisbaneDateElement = document.querySelector("#brisbane-date");
   let brisbaneAmPmElement = document.querySelector("#brisbane-am-pm");
   let brisbaneTime = moment().tz("Australia/Brisbane").format("h:mm:ss");
-  let brisbaneDate = moment()
-    .tz("Australia/Brisbane")
-    .format("ddd, MMM D, YYYY");
+  let brisbaneDay = moment().tz("Australia/Brisbane").format("ddd, ");
+  let brisbaneDate = moment().tz("Australia/Brisbane").format("MMM D, YYYY");
   let brisbaneAmPm = moment().tz("Australia/Brisbane").format("A");
   let brisbaneTodayElement = document.querySelector("#brisbane-today");
-  let userTime = moment().format("ddd, MMM D, YYYY");
+  let localDate = moment().format("MMM D, YYYY");
 
-  if (brisbaneDate === userTime) {
+  if (brisbaneDate === localDate) {
     brisbaneTodayElement.innerHTML = "Today";
   } else {
-    if (brisbaneDate < userTime) {
+    if (brisbaneDate < localDate) {
       brisbaneTodayElement.innerHTML = "Yesterday";
     } else {
-      if (brisbaneDate > userTime) {
+      if (brisbaneDate > localDate) {
         brisbaneTodayElement.innerHTML = "Tomorrow";
       }
     }
   }
 
   brisbaneTimeElement.innerHTML = brisbaneTime;
+  brisbaneDayElement.innerHTML = brisbaneDay;
   brisbaneDateElement.innerHTML = brisbaneDate;
   brisbaneAmPmElement.innerHTML = brisbaneAmPm;
 }, 1000);
